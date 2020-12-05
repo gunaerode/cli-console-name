02ef51a30c4c8e9fad5a81e4765fd17dd558f2f0
gunaerode <guna.learn2016@gmail.com>
2020-12-05 16:34:17 +0530

    init(master): initial commit :smiley: :fire:

---
+++ index.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+console.log(`
+
+Hey,
+
+I'm Guna 🤪 
+
+
+    Senior Software Engineer at SST India Pvt Ltd., 
+I have been working last 4 and half years in Bangalore.
+as a Fullstack developer (MERN).
+
+Javascript enthusiastic and Programming nerd :smiley:
+
+📖 Github:  https://github.com/gunaerode
+🐦 Twitter: https://twitter.com/Gunasek55186393
+
+`);
